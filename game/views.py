5eda41d125a9030b70e002b975bff3271afe66e4
d.kovriga <d.kovriga@securitycode.ru>
2017-05-19 15:45:16 +0300

    quest view

--- game/views.py
+++ game/views.py
@@ -1,5 +1,14 @@
 from aiohttp import web
 
+from game.models import quest
+
 
 async def index(request):
     return web.Response(text='Hello Aiohttp!')
+
+class Quests(web.View):
+    async def get(self):
+        return web.json_response(data)
+
+    async def post(self):
+        return web.json_response(data)
